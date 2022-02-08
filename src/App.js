d021d5d9f39f24c32058c1387c056f4c3c4155bb
Bharath Gunasekaran <Bharathgunasekaran@AGLS-MLT-430s-MacBook-Air.local>
2022-02-08 14:44:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [Name,setName] = useState({firstname: '',lastname: '',email:'',gender:'',courses:'' });
+  const [error,setError] = useState({firstname: '',lastname: '',email:'',gender:'',courses:'' });
+  
+  const onChangeName = (e) => {
+    if (e.target.value ==="")
+    { 
+      setError({ ...error,[e.target.name]: `${e.target.name} is required`});
+    }
+    else
+    {
+      setError({ ...error,[e.target.name]:""})
+    }
+    setName({ ...Name, [e.target.name]: e.target.value});
+  }
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    console.log(Name)
+  }
+   return (
+    <>
+    <h3>Controlled Forms</h3>
+    <form onSubmit ={(e) => onSubmit(e)}>
+      <div>
+        <label> First Name : </label>
+        <input 
+        type='text' 
+        required
+        name='firstname'
+        value={Name.firstname}
+        onChange={(e) => onChangeName(e)}/>{' '}
+        <span style={{ color:"red"}}>{error.firstname}</span>
+      </div>
+      <br/>
+      
+      <div>
+        <label> Last Name : </label>
+        <input 
+        type='text' 
+        name='lastname' 
+        value={Name.lastname}
+        required
+        onChange={(e) => onChangeName(e)}/>
+        <span style={{ color: "red" }}>{error.lastname}</span>
+      </div>
+      <br/>
+
+
+      <div>
+        <label> Email : </label>
+        <input 
+        type='text' 
+        required
+        name='email' 
+        value={Name.email}
+        onChange={(e) => onChangeName(e)}/>{' '}
+        <span style={{ color: "red" }}>{error.email}</span>
+      </div>
+      <br/>
+
+      <div>
+        <label> Gender : </label>
+        <input 
+        type='radio' 
+        name='gender' 
+        value='male'
+        onChange={(e) => onChangeName(e)}/>Male
+        <input 
+        type='radio' 
+        name='gender'
+        value='female'
+        onChange={(e) => onChangeName(e)}/>Female{' '}
+        <span style={{ color: "red" }}>{error.gender}</span>
+      </div>
+      <br/>
+
+
+      <div>
+        <label> Courses : </label>
+        <select name="courses" onChange={(e) => onChangeName(e)}>
+          <option> </option>
+          <option>React.js</option>
+          <option>Node.js</option>
+          <option>Express.js</option>
+          <option>Mongo</option>
+        </select>{' '}
+        <span style={{ color: "red" }}>{error.courses}</span>
+      </div>
+      <br/>
+
+
+      <div>
+        <button type='submit'> Submit </button> &nbsp;
+        <button type='button' onClick={onChangeName}> Reset </button> &nbsp;
+      </div>
+    </form>
+    </>
   );
 }
 
